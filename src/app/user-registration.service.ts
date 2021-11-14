e20ca3932cc0c6f8a3242da7630369113ef6fe30
GUMMAGUTTA_NARESH <naresh.g@thresholdsoft.com>
2021-11-14 14:33:08 +0530

    base url added

--- src/app/user-registration.service.ts
+++ src/app/user-registration.service.ts
@@ -7,11 +7,18 @@ import { User } from './user';
 })
 export class UserRegistrationService {
 
+  baseUrl:string = "http://localhost:8080/";
+  baseAwsUrl:string = "http://ec2-3-134-78-107.us-east-2.compute.amazonaws.com:8080/";
   constructor(private http: HttpClient) { }
 
-  public doRegistration(user:User){
-    // return this.http.post("http://localhost:8080/api/users",user, {responseType:'text' as 'json'});
-    return this.http.post("ec2-3-134-78-107.us-east-2.compute.amazonaws.com:8080/api/users",user, {responseType:'text' as 'json'});
+  public doRegistration(user:User, urlType:boolean){
+    if(urlType){
+      return this.http.post(this.baseAwsUrl+"api/users",user, {responseType:'text' as 'json'});
+    }else{
+      return this.http.post(this.baseUrl+"api/users",user, {responseType:'text' as 'json'});
+    }
+    
+   
   }
 
   public sendEmail(map:Map<String,Object>){
